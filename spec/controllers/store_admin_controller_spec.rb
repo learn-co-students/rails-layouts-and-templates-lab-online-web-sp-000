05878ec2efd1d4d77ba0911bef3eacaa3b1cc661
Eric Clark <eclark0013@gmail.com>
2019-12-16 12:39:06 -0500

    Done.

--- spec/controllers/store_admin_controller_spec.rb
+++ spec/controllers/store_admin_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe StoreAdminController, type: :controller do
+# RSpec.
+describe StoreAdminController, type: :controller do
   include Capybara::DSL
   render_views
 
