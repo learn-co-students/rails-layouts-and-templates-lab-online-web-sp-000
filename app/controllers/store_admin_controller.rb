ad6d4164680cb02ad79f79f61cbef77bd736bf95
nathan-stamand <nathansaint810@gmail.com>
2020-05-12 15:42:56 -0500

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,16 @@
+class StoreAdminController < ApplicationController
+    layout "layouts/admin"
+
+    def home
+
+    end
+
+    def orders
+        render :layout => "layouts/order_administration"
+    end
+
+    def invoice 
+        render :layout => false
+    end
+
+end
