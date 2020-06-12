35f8d7ea237f09b004a1766e84f0973f3d05bffa
dkw5011 <dkw5011@gmail.com>
2020-06-12 11:24:57 -0400

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,17 @@
+class StoreAdminController < ApplicationController
+    def admin
+    end
+
+    def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
+    end
+
+    def home
+        render :layout => "admin"
+    end
+
+end
