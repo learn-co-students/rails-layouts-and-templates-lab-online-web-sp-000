8791a4559ed2f8e683ed1f3e945cd59f489a60ad
chienleow <chienleow1@gmail.com>
2020-08-15 19:16:21 -0400

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,5 +1,15 @@
 class StoreAdminController < ApplicationController
-    
-    layout 'admin'
+    layout "admin"
 
+    def home
+        # this method can be removed because by default Rails will create /about /home and /features methods
+    end
+
+    def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
+    end
 end
